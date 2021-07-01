c8bd8c51ade657c17e98de9d413602eb39749cc9
Nargis <nargis.salim@btinternet.com>
2021-07-01 17:38:26 +0100

    invoke and print players plus invoke games

--- teamStats.js
+++ teamStats.js
@@ -3,54 +3,90 @@
 
 
 // create data using data structures such as objects, arrays, etc. and then, manipulate them, as well as, gain insights from them.
- 
 
+
+console.log('TEAM STATS\n')
 // the team has players, and it plays games
 const team = {
-    _players: [
-      {firstName: 'Tom',
-      lastName: 'Good',
-      age: 32},
-      {firstName: 'Margo',
-      lastName: 'Leadbetter',
-      age: 35},
-      {firstName: 'Barbera',
-      lastName: 'Good',
-      age: 28},
-      {firstName: 'Jerry',
-      lastName: 'Leadbetter',
-      age: 28}
-      ],
-  
-  
-    _games: [
-      {game: 'Aces',
-      chaliesAngels: 40,
-      theGoodLife: 30},
-      {game: 'Conquerors',
-     chaliesAngels: 30,
-      theGoodLife: 15},
-      {game: 'Assassins',
-     chaliesAngels: 15,
-      theGoodLife: 30}
-      ],
-  
-  
-  // has getter methods for _players and _games keys. 
-  get players(){
-  return this._players;},
-  
-  get games(){
-  return this._games;
-  },
-  
-  
-  // add a new player to team
-  addPlayer(firstName, lastName, age){
-    let player = {
-      firstName: firstName,
-      lastName: lastName,
-      age: age};
-    this.players.push(player);
-  },
-  }
+  _players: [
+    {firstName: 'Sabrina',
+    lastName: 'Duncan',
+    age: 24},
+    {firstName: 'Jill',
+    lastName: 'Munroe',
+    age: 23},
+    {firstName: 'Kelly',
+    lastName: 'Garrett',
+    age: 25},
+    {firstName: 'John',
+    lastName: 'Bosley',
+    age: 46}
+    ],
+
+  _games: [
+    {opponent: 'Aces',
+    home: 40,
+    rival: 30},
+    {opponent: 'Conquerors', 
+    home: 30,
+    rival: 15},
+    {opponent: 'Assassins',
+    home: 15,
+    rival: 30}
+    ],
+
+
+// has getter methods for _players and _games keys. 
+get players(){
+return this._players;},
+
+get games(){
+return this._games;
+},
+
+
+// add a new player to team
+addPlayer(firstName, lastName, age){
+  let player = {
+    firstName: firstName,
+    lastName: lastName,
+    age: age};
+  this.players.push(player);
+},
+
+
+// add a new game for the team to play
+addGame(rivalTeamName, homePoints, rivalPoints){
+  const game = {
+    opponent: rivalTeamName,
+    home: homePoints,
+    rival: rivalPoints
+    };
+  this.games.push(game);
+},
+}
+
+
+// invoke new player(s)
+team.addPlayer('Natalie', 'Cook', 26);
+team.addPlayer('Dylan', 'Sanders', 21);
+team.addPlayer('Alex', 'Munday', 22);
+console.log(team.players);
+// Output:
+// TEAM STATS
+
+// [ { firstName: 'Sabrina', lastName: 'Duncan', age: 24 },
+//   { firstName: 'Jill', lastName: 'Munroe', age: 23 },
+//   { firstName: 'Kelly', lastName: 'Garrett', age: 25 },
+//   { firstName: 'John', lastName: 'Bosley', age: 46 },
+//   { firstName: 'Natalie', lastName: 'Cook', age: 26 },
+//   { firstName: 'Dylan', lastName: 'Sanders', age: 21 },
+//   { firstName: 'Alex', lastName: 'Munday', age: 22 } ]
+
+// // Invoke new game results and print the team‘s updated games array
+team.addGame('Majestics', 40, 40);
+team.addGame('Saints', 15, 40);
+team.addGame('Titans', 30 , 15);
+// console.log(team.games);
+// Output
+
