686a62b43d79bd974c6e1fe6d3cfe532d899a0b3
Nargis <nargis.salim@btinternet.com>
2021-07-01 14:06:34 +0100

    first commit

---
+++ teamStats.js
@@ -0,0 +1,8 @@
+// Team Stats
+// To create and extract information about a sports team. 
+
+
+// create data using data structures such as objects, arrays, etc. and then, manipulate them, as well as, gain insights from them.
+ 
+
+// the team has players, and it plays games
